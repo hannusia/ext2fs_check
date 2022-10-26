4a1e8a7eeb245bca71edb4dd0e680c8c0e331dfe
hannusia <hanna.yershova@ucu.edu.ua>
2022-10-26 20:10:18 +0300

    initial partition info read

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/fs.h>
+#include <ext2fs/ext2_fs.h>
+
+#define BASE_OFFSET 1024                   /* locates beginning of the super block (first group) */
+#define FD_DEVICE "/dev/nvme0n1p3"               /* name of your ext2fs partition */
+
+static unsigned int block_size = 0;        /* block size (to be calculated) */
+
+int main(void)
+{
+	struct ext2_super_block super;
+	struct ext2_group_desc group;
+	int fd;
+
+	/* open floppy device */
+
+	if ((fd = open(FD_DEVICE, O_RDONLY)) < 0) {
+		perror(FD_DEVICE);
+		exit(1);  /* error while opening the floppy device */
+	}
+
+	/* read super-block */
+
+	lseek(fd, BASE_OFFSET, SEEK_SET); 
+	read(fd, &super, sizeof(super));
+
+	if (super.s_magic != EXT2_SUPER_MAGIC) {
+		fprintf(stderr, "Not a Ext2 filesystem\n");
+		exit(1);
+	}
+		
+	block_size = 1024 << super.s_log_block_size;
+
+	/* read group descriptor */
+
+	lseek(fd, BASE_OFFSET + block_size, SEEK_SET);
+	read(fd, &group, sizeof(group));
+	close(fd);
+
+	printf("Reading first group-descriptor from device " FD_DEVICE ":\n"
+	       "Blocks bitmap block: %u\n"
+	       "Inodes bitmap block: %u\n"
+	       "Inodes table block : %u\n"
+	       "Free blocks count  : %u\n"
+	       "Free inodes count  : %u\n"
+	       "Directories count  : %u\n"
+	       ,
+	       group.bg_block_bitmap,
+	       group.bg_inode_bitmap,
+	       group.bg_inode_table,
+	       group.bg_free_blocks_count,
+	       group.bg_free_inodes_count,
+	       group.bg_used_dirs_count);    /* directories count */
+	
+	exit(0);
+}
